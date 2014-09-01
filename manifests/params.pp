f940e2c57d139dc9d8f6eac42dbaf5a45d93d267
Russell McGregor <russell.mcgregor@brisbane.qld.gov.au>
2014-09-01 19:14:47 +1000

    update for squid3 dir naming and modify params format

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,35 +1,35 @@
 # Params class
 class squid::params {
-  $localnet_src = '10.0.0.0/8',
-  $cache_mem = '256 MB',
-  $maximum_object_size_in_memory = '512 KB',
-  $memory_replacement_policy = 'lru',
-  $cache_replacement_policy = 'lru',
-  $cache_dir = '/var/spool/squid',
-  $cache_dir_type = 'ufs',
-  $cache_dir_size = 100,
-  $maximum_object_size = '4096 KB',
-  $cache_swap_low = 90,
-  $cache_swap_high = 95,
-  $log_fqdn = off,
-  $cachemgr_passwd = disable,
-  $visible_hostname = undef,
-  $snmp_port = undef,
-  $refresh_pattern = undef,
-  $negative_ttl = undef,
-  $cache_peers = undef,
+  $localnet_src = '10.0.0.0/8'
+  $cache_mem = '256 MB'
+  $maximum_object_size_in_memory = '512 KB'
+  $memory_replacement_policy = 'lru'
+  $cache_replacement_policy = 'lru'
+  $cache_dir = '/var/spool/squid'
+  $cache_dir_type = 'ufs'
+  $cache_dir_size = 100
+  $maximum_object_size = '4096 KB'
+  $cache_swap_low = 90
+  $cache_swap_high = 95
+  $log_fqdn = off
+  $cachemgr_passwd = disable
+  $visible_hostname = undef
+  $snmp_port = undef
+  $refresh_pattern = undef
+  $negative_ttl = undef
+  $cache_peers = undef
   case $::operatingsystem {
     ubuntu,debian: {
-      $squid_package = 'squid',
-      $squid_service = 'squid',
-      $squid_user = 'proxy',
-      $squid_group = 'proxy',
+      $squid_package = 'squid'
+      $squid_service = 'squid'
+      $squid_user = 'proxy'
+      $squid_group = 'proxy'
     }
     default: {
-      $squid_package = 'squid',
-      $squid_service = 'squid',
-      $squid_user = 'proxy',
-      $squid_group = 'proxy',
+      $squid_package = 'squid'
+      $squid_service = 'squid'
+      $squid_user = 'proxy'
+      $squid_group = 'proxy'
     }
-
+  }
 }
