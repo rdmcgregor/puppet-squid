2de6b47b2eac13d31f728be84c79e342c9b454a3
Russell McGregor <russell.mcgregor@brisbane.qld.gov.au>
2014-09-01 20:03:32 +1000

    Initial configuration for Hiera support

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,25 @@
 #
 # Copyright 2014 Brisbane City Council, unless otherwise noted.
 #
-class squid inherits squid::params {
+class squid (
+  $localnet_src 			= $squid::params::localnet_src, 
+  $cache_mem 				= $squid::params::cache_mem, 
+  $maximum_object_size_in_memory 	= $squid::params::maximum_object_size_in_memory, 
+  $memory_replacement_policy 		= $squid::params::memory_replacement_policy,
+  $cache_replacement_policy 		= $squid::params::cache_replacement_policy,
+  $cache_dir 				= $squid::params::cache_dir,
+  $cache_dir_type 			= $squid::params::cache_dir_type,
+  $cache_dir_size 			= $squid::params::cache_dir_size,
+  $maximum_object_size 			= $squid::params::maximum_object_size,
+  $cache_swap_low 			= $squid::params::cache_swap_low,
+  $cache_swap_high 			= $squid::params::cache_swap_high,
+  $cachemgr_passwd 			= $squid::params::cachemgr_passwd,
+  $visible_hostname 			= $squid::params::visible_hostname,
+  $snmp_port 				= $squid::params::snmp_port,
+  $refresh_pattern 			= $squid::params::refresh_pattern,
+  $negative_ttl 			= $squid::params::negative_ttl,
+  $cache_peers 				= $squid::params::cache_peers
+) inherits squid::params {
   include stdlib
 
   anchor { 'squid::begin': }
