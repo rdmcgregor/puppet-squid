c32a7a357ef460e0079f1be72344127b7f6cc0be
Russell McGregor <russell.mcgregor@brisbane.qld.gov.au>
2014-09-01 18:47:50 +1000

    Added package information and actaully set up something to do

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,6 +36,12 @@
 # Copyright 2014 Brisbane City Council, unless otherwise noted.
 #
 class squid {
+  include stdlib
+  include squid::params
 
-
+  anchor { 'squid::begin': } 
+  -> class { 'squid::package': }
+  -> class { 'squid::config': }
+  ~> class { 'squid::service': }
+  -> anchor { 'squid::end': }
 }
---
+++ manifests/package.pp
@@ -0,0 +1,19 @@
+# == Class: squid::package
+#
+#  Service class associated with Squid module
+#
+# === Authors
+#
+# Russell McGregor <rdmcgregor@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Brisbane City Council, unless otherwise noted.
+#
+
+
+class squid::package {
+  package { $squid::params::package:
+    ensure => 'present',
+  }
+}
